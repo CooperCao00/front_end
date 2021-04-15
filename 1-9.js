c0309eab42294e0a92782b9b225bc8feceeb4e20
cooper <cycle980216@163.com>
2021-04-15 23:11:36 +0800

    feat 增加server注释

--- 1-9.js
+++ 1-9.js
@@ -1,5 +1,7 @@
+// 1. 通过require("http")来引入http对象
 const http = require("http");
 
+// 2. 利用http自带方法createServer启动一个后台服务
 http.createServer((req, res) => {
     let body = [];
     req.on("error", err => {
@@ -7,11 +9,13 @@ http.createServer((req, res) => {
     }).on("data", chunk => {
         body.push(chunk.toString());
     }).on("end", () => {
+        // Buffer类作为是处理二进制数据的缓冲区
         body = Buffer.concat(body).toString();
         console.log("body:", body);
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(" Hello world\n");
     })
+// .listen(8088)让服务器监听localhost:8088端口
 }).listen(8088)
 
 console.log("server started");
