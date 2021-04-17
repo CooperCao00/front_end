0f1a5ddaad7f6b91c0a27e5d99c641b1c7ca0932
cooper <cycle980216@163.com>
2021-04-17 11:30:30 +0800

    feat send 函数编写

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const http = require("http");
 // 2. 利用http自带方法createServer启动一个后台服务
 http.createServer((req, res) => {
     let body = [];
+    let body1 = [];
     req.on("error", err => {
         console.log(err);
     }).on("data", chunk => {
-        body.push(chunk.toString());
+
+        console.log("chunk:\n", chunk);
+        // body.push(chunk.toString());
+        body.push(chunk);
+        body1.push(chunk.toString())
+
+        console.log("body pushed:", body);
     }).on("end", () => {
+
+        console.log("end");
         // Buffer类作为是处理二进制数据的缓冲区
         body = Buffer.concat(body).toString();
+        // body1 = Buffer.concat(body).toString();
         console.log("body:", body);
+        // console.log("body1: ", body1);
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(" Hello world\n");
     })
